f1e59aee120d5f9298642c431ddb801322eb4a91
PS12106390 <PS12106390@grupoamil.com.br>
2024-03-07 11:57:05 -0300

    Changing return message

--- src/services/SearchUserService.ts
+++ src/services/SearchUserService.ts
@@ -15,7 +15,7 @@ export class SearchUserService {
       return {
         error: true,
         data: null,
-        message: "Usuário e/ou senha inválidos.",
+        message: "Usuário não encontrado.",
       };
     }
 
